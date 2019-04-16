dd0c2dbbc5622c3a67428025d4456fa6322288c0
Anton Filiptsov <antonfiliptsov@BrushedBit-2.local>
2019-04-16 12:28:14 -0500

    dummy todo

--- src/App.js
+++ src/App.js
@@ -3,23 +3,58 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    list: [
+    ],
+    strValue: ''
+  }
+
+  addElementToTodo = () => {
+    let newArr = [...this.state.list];
+    newArr.push({ title: this.state.strValue, id: Date() })
+
+
+
+    this.setState({
+      list: newArr
+    })
+  }
+
+  onChangeHandler = (e) => {
+    this.setState({
+      strValue: e.target.value
+    })
+    console.log(this.state.strValue)
+  }
+
+  handleRemove(id) {
+    let newArr = [...this.state.list]
+    let remainder = newArr.filter((el) => {
+      if (el.id !== id) return id;
+    })
+    console.log(remainder)
+    // Update state with filter
+    this.setState({ list: remainder });
+  }
+
+
   render() {
+    console.log(this.state.list)
+    let mapOverTodo = this.state.list.map((el, i) => {
+      return (
+        <p key={i} onClick={() => this.handleRemove(el.id)}>{el.title}</p>
+      )
+    })
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>What do you want to do?</div>
+        <div style={{ display: 'block' }}>
+          {mapOverTodo}
+        </div>
+        <input type="text" onChange={(e) => this.onChangeHandler(e)} placeholder="Add something here"></input>
+        <button onClick={this.addElementToTodo}>Submit</button>
       </div>
     );
   }
